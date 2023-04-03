527fec99f134d5e4e0fea15a55bcf5b1493bbbd9
prashanth234 <prashanthbodduna4@gmail.com>
2023-04-04 00:41:22 +0530

    initiated frontend project

---
+++ frontend/src/main.ts
@@ -0,0 +1,59 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router';
+
+import { IonicVue } from '@ionic/vue';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/vue/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/vue/css/normalize.css';
+import '@ionic/vue/css/structure.css';
+import '@ionic/vue/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/vue/css/padding.css';
+import '@ionic/vue/css/float-elements.css';
+import '@ionic/vue/css/text-alignment.css';
+import '@ionic/vue/css/text-transformation.css';
+import '@ionic/vue/css/flex-utils.css';
+import '@ionic/vue/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+/* Graphql */
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'http://localhost:8000/graphql',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
+import { createApolloProvider } from '@vue/apollo-option'
+
+const apolloProvider = createApolloProvider({
+  defaultClient: apolloClient,
+})
+
+const app = createApp(App)
+  .use(IonicVue)
+  .use(router)
+  .use(apolloProvider);
+
+app.config.globalProperties.$apolloClient = apolloClient
+  
+router.isReady().then(() => {
+  app.mount('#app');
+});
---
+++ frontend/src/views/LoginPage.vue
@@ -0,0 +1,41 @@
+<template>
+    <h1>hello</h1>
+    <div>{{  allCategories }}</div>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+
+export default {
+    apollo: {
+        allCategories: gql`query {
+                    allCategories {
+                        name
+                    }
+                }`,
+    },
+    data () {
+        return { 
+            allCategories: []
+        }
+    },
+    methods: {
+        // async getdata () {
+        //     let data = await this.$apolloClient.query({
+        //         query: gql `
+        //             query allCategories {
+        //                 name
+        //             }
+        //         `
+        //     })
+        //     console.log(data)
+        // }
+    },
+    mounted () {
+        // this.getdata()
+        setTimeout(() => {
+            console.log(this.allCategories)
+        }, 4000);
+    }
+}
+</script>
